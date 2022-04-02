663e524ddb43dff68f3d3cc83fece194fe54f1e0
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-04-02 22:37:05 +0900

    site-test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
-class Site {}
+class Site {
+    constructor(text) {
+        this.boards = [];
+    }
+
+    addBoard(noticeBoard) {
+        if (this.boards.find(() => text === noticeBoard)) {
+            throw new Error();
+        };
+        this.boards.push(noticeBoard);
+        console.log(noticeBoard)
+    }
+
+    findBoardByName() {
+        return Object(this.boards)[0]
+    }   
+}
 
 class Board {}
 
