a0b31b9b049621c3d46f10a71ff0b76bb3aabec7
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-04-04 03:17:17 +0900

    site-test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 class Site {
-    constructor(text) {
+    constructor() {
         this.boards = [];
     }
 
     addBoard(noticeBoard) {
-        if (this.boards.find(() => text === noticeBoard)) {
+        if (this.boards.find((a) => a.name === noticeBoard.name)) {
             throw new Error();
         };
+        noticeBoard.tr = true
         this.boards.push(noticeBoard);
-        console.log(noticeBoard)
     }
 
     findBoardByName() {
@@ -16,11 +16,84 @@ class Site {
     }   
 }
 
-class Board {}
+class Board extends Site {
+    constructor(name) {
+        super()
+        this.article = [];
+        this.name = name;
 
-class Article {}
+        if(this.name === "" || this.name === null) {
+            throw new Error();
+        }
+        
+        
+        //Article은 독립적인 글을 작성할 때 사용한다.
 
-class Comment {}
+    }
+    publish(article) {
+        if (this.tr) {
+            article.tr = true
+            article.id = `${this.name}-${Math.random()}`
+            article.createdDate = new Date().toISOString();
+            //iso => parseInt(new Date(iso).getTime() / 1000);
+            this.article.push(article);
+        } else throw new Error();
+        
+        console.log(this.boards)
+    };
+
+    
+    getAllArticles() {
+        return Object(this.article)
+    }
+}
+
+
+
+
+
+class Article extends Board {
+    constructor(article) {
+        super();
+        // this.subject = subject;
+        // this.content = content;
+        // this.author = author;
+        this.comment = [];
+
+        // if ( !article.subject || !article.content || !article.author ) {
+        //     throw new Error();
+        // }
+        
+        // if( article.subject === "" || article.subject === null || 
+        //     article.content === "" || article.content === null ||
+        //     article.author === "" || article.author === null) { 
+        //         throw new Error();
+        // }
+
+    }
+    reply(comment) {
+        if(this.tr) {
+            comment.tr = true
+            comment.createdDate = new Date().toISOString();
+            this.comment.push(comment);
+        }else throw new Error();
+
+    
+    }
+    getAllComments() {
+        return Object(this.comment)
+    }
+}
+
+class Comment extends Article{
+    constructor(comment) {
+        super();
+        if( comment.content === "" || comment.content === null ||
+            comment.author === "" || comment.author === null) {
+                throw new Error();
+            }
+    }
+}
 
 module.exports = {
     Site,
